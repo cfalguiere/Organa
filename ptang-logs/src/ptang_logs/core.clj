47b5f21c2fa1cb05509010b4e4cfc81f536925c5
Claude Falguiere <cfalguiere@gmail.com>
2013-12-24 00:09:56 +0100

    incanter stats

--- ptang-logs/src/ptang_logs/core.clj
+++ ptang-logs/src/ptang_logs/core.clj
@@ -1,6 +1,13 @@
 (ns ptang-logs.core
-  (:require [clojure.java.io :as io])
+  (:use [incanter.core :only [$ $data $rollup col-names conj-cols save]])
+  (:use [incanter.stats :only [mean sd quantile]])
+  (:require [clojure.java.io :as io]
+	    [incanter.core :as incanter]
+	    [incanter.io :as incanterio])
   (:import java.util.regex.Pattern))
+
+;; chemin de readlines
+;;http://blog.magpiebrain.com/tag/incanter/
 
 (def pattern #"^([0-9- :,]*) INFO.*TIME MONITOR.*;(.*);(.*) ms$")
 
@@ -28,6 +35,47 @@
    (with-open [rdr (io/reader filename)]
        (doall (map parse-line (line-seq rdr))))))
 
-(defn -main [& args]
- (println (parse-file  "sample.log")))
+(defn response-time-summary
+  "build the summary of a dataset"
+  [ds] 
+   (zipmap [ :count :mean :sd :min :q95 :max]
+      (flatten (incanter/with-data ($ :duration ds)
+	   [ (count $data) (mean $data) (sd $data) (quantile $data :probs[0 0.95 1]) ] ))))
 
+(defn readings-to-dataset
+  "build a dataset from a list of readings"
+  [readings]
+  (incanter/dataset [:timestamp :label :duration] readings))
+
+(defn compute-metric
+  "compute a metric grouped by label"
+  [ds [metric f]]
+  (col-names ($rollup f :duration :label ds) [:label metric])) 
+
+(defn q [p ds] (quantile ds :probs [p])) 
+
+(defn compute-stats
+  "compute stats grouped by label"
+  [ds]
+  (let [metrics {:count count :mean mean :sd sd
+		 :min (partial q 0) :q95 (partial q 0.95) :max (partial q 1)}]
+	 (map (partial compute-metric ds) metrics)))
+
+(defn stats
+   [ds]
+   (let [result (compute-stats ds)]
+     (col-names 
+      (conj-cols
+       ($ :label (nth result 0)) 
+       ($ :count (nth result 0)) 
+       ($ :mean (nth result 1)) 
+       ($ :sd (nth result 2)) 
+       ($ :min (nth result 3)) 
+       ($ :q95 (nth result 4)) 
+       ($ :max (nth result 5)) )
+      [:label :count :mean :sd :min :q95 :max])))
+  
+(defn -main [filename]
+   (incanter/save (stats
+	  (readings-to-dataset (parse-file  filename))) "stats.csv")) 
+ 
