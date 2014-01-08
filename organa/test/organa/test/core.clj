6efd57684c18f274b7c1ada13626b3f2fb35028c
Claude Falguiere <cfalguiere@gmail.com>
2014-01-08 02:44:49 +0100

    fixed time parser test

--- organa/test/organa/test/core.clj
+++ organa/test/organa/test/core.clj
@@ -1,9 +1,12 @@
 (ns organa.test.core
   (:use [organa.core])
+  (:use [organa.common])
   (:use [clojure.test])
   (:use [midje.sweet]))
 
 (def line-source "2013-12-13 08:00:00,924 INFO  [STDOUT] (http-10.12.20.22-17011-1) 08:00:00.923 INFO  f.a.t.aspect.LogServiceAspect - [TIME MONITOR] 08:00:00;RS_OW_AgencyDataSupplierService;208 ms")
 
-;(fact "it should extract service name and duration"
- ;     (parse-line line-source) => ["2013-12-13 08:00:00,924" "RS_OW_AgencyDataSupplierService" 208])
+(fact "it should return a reading with 2 columns"
+      (parse-line time-pattern time-to-reading line-source)
+      => ["2013-12-13 08:00:00,924" "RS_OW_AgencyDataSupplierService" 208])
+
